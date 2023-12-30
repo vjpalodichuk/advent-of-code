9dbf12cce57c893673aba657f42dd68b4193664c
Vincent Palodichuk <vincent@palodichuk.com>
2023-12-30 16:49:11 -0600

    Prepare for Day 7!

---
+++ aoc-2015/src/test/java/com/capital7software/aoc2015/days/Day07Test.java
@@ -0,0 +1,45 @@
+package com.capital7software.aoc2015.days;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day07Test {
+    List<String> lines;
+    Path path;
+
+    @BeforeEach
+    void setUp() {
+        var instance = new Day07();
+        setupFromFile(instance.getDefaultInputFilename());
+    }
+
+    void setupFromFile(String filename) {
+        try {
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            var url = classloader.getResource(filename);
+            assert url != null;
+            path = Paths.get(url.toURI());
+            lines = Files.readAllLines(path);
+        } catch (URISyntaxException | IOException e) {
+            System.out.printf("Unable to load input data from: %s%n", path);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void whatSignalsAreOnTheWires() {
+        var expectedCount = 998_996;
+        var actualCount = 998_996;
+
+        assertEquals(expectedCount, actualCount);
+    }
+}

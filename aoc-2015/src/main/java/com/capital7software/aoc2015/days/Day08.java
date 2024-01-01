664e9eaf8fd33fe553df07f346c14687ddd23953
Vincent Palodichuk <vincent@palodichuk.com>
2024-01-01 07:57:36 -0600

    Day 8 Complete!

--- aoc-2015/src/main/java/com/capital7software/aoc2015/days/Day08.java
+++ aoc-2015/src/main/java/com/capital7software/aoc2015/days/Day08.java
@@ -1,9 +1,11 @@
 package com.capital7software.aoc2015.days;
 
 import com.capital7software.aoc2015.lib.AdventOfCodeSolution;
+import com.capital7software.aoc2015.lib.string.EncodingFun;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * --- Day 8: Matchsticks ---
@@ -36,7 +38,25 @@ import java.util.List;
  * code (2 + 5 + 10 + 6 = 23) minus the total number of characters in memory for string
  * values (0 + 3 + 7 + 1 = 11) is 23 - 11 = 12.
  * <p>
- *
+ *     Your puzzle answer was 1350.
+ * <p>
+ * --- Part Two ---
+ * Now, let's go the other way. In addition to finding the number of characters of code, you should now encode
+ * each code representation as a new string and find the number of characters of the new encoded representation,
+ * including the surrounding double quotes.
+ * <p>
+ * For example:
+ * <p>
+ * "" encodes to "\"\"", an increase from 2 characters to 6.
+ * "abc" encodes to "\"abc\"", an increase from 5 characters to 9.
+ * "aaa\"aaa" encodes to "\"aaa\\\"aaa\"", an increase from 10 characters to 16.
+ * "\x27" encodes to "\"\\x27\"", an increase from 6 characters to 11.
+ * Your task is to find the total number of characters to represent the newly encoded strings minus the
+ * number of characters of code in each original string literal. For example, for the strings above,
+ * the total encoded length (6 + 9 + 16 + 11 = 42) minus the characters in the original code
+ * representation (23, just like in the first part of this puzzle) is 42 - 23 = 19.
+ * <p>
+ *     Your puzzle answer was 1350.
  * <p>
  */
 public class Day08 implements AdventOfCodeSolution {
@@ -47,11 +67,53 @@ public class Day08 implements AdventOfCodeSolution {
 
     @Override
     public void runPart1(List<String> input) {
+        var memory = new AtomicInteger(0);
+        var code = new AtomicInteger(0);
+
         var start = Instant.now();
-        var total = 0;
+
+        input.forEach(s -> {
+            memory.set(memory.get() + memoryCount(s));
+            code.set(code.get() + codeCount(s));
+        });
+
+        var total = code.get() - memory.get();
         var end = Instant.now();
         System.out.printf(
-                "%d is provided to wire a!%n", total);
+                "The number of characters of code: %d minus the number of characters" +
+                        " in memory %d in the whole file is: %d%n", code.get(), memory.get(), total);
         printTiming(start, end);
+    }
+
+    @Override
+    public void runPart2(List<String> input) {
+        var newCode = new AtomicInteger(0);
+        var code = new AtomicInteger(0);
+
+        var start = Instant.now();
+
+        input.forEach(s -> {
+            newCode.set(newCode.get() + codeCount(encodeString(s)));
+            code.set(code.get() + codeCount(s));
+        });
+
+        var total = newCode.get() - code.get();
+        var end = Instant.now();
+        System.out.printf(
+                "The number of characters in new string: %d minus the number of characters" +
+                        " of code in old string %d in the whole file is: %d%n", newCode.get(), code.get(), total);
+        printTiming(start, end);
+    }
+
+    public int codeCount(String string) {
+        return EncodingFun.countCode(string);
+    }
+
+    public int memoryCount(String string) {
+        return EncodingFun.countMemory(string);
+    }
+
+    public String encodeString(String string) {
+        return EncodingFun.encodeString(string);
     }
 }
--- aoc-2015/src/test/java/com/capital7software/aoc2015/days/Day08Test.java
+++ aoc-2015/src/test/java/com/capital7software/aoc2015/days/Day08Test.java
@@ -1,6 +1,7 @@
 package com.capital7software.aoc2015.days;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -8,6 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day08Test {
     List<String> lines;
@@ -31,4 +34,38 @@ class Day08Test {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    void codeCountOfStrings() {
+        var instance = new Day08();
+        var expected = new int[] {2, 5, 10, 6, 43};
+
+        for (int i = 0; i < lines.size(); i++) {
+            var actual = instance.codeCount(lines.get(i));
+            assertEquals(expected[i], actual, "Code count didn't match for: " + lines.get(i));
+        }
+    }
+
+    @Test
+    void codeMemoryOfStrings() {
+        var instance = new Day08();
+        var expected = new int[] {0, 3, 7, 1, 29};
+
+        for (int i = 0; i < lines.size(); i++) {
+            var actual = instance.memoryCount(lines.get(i));
+            assertEquals(expected[i], actual, "Memory count didn't match for: " + lines.get(i));
+        }
+    }
+
+    @Test
+    void codeCountOfNewStrings() {
+        var instance = new Day08();
+        var expected = new int[] {6, 9, 16, 11, 56};
+
+        for (int i = 0; i < lines.size(); i++) {
+            var actual = instance.codeCount(instance.encodeString(lines.get(i)));
+            assertEquals(expected[i], actual, "Code count didn't match for: " + lines.get(i));
+        }
+    }
+
 }

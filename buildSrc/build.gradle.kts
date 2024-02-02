c1a6472f7f61c253b6d722e387dd7f8320ac40c9
Vincent Palodichuk <vincent@palodichuk.com>
2024-02-02 03:43:13 -0600

    :bug: Resolved Artifactory and gradle 9 compatibility issues.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains:annotations:24.0.1")
     implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.0.3")
-    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4+")
+    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:5+")
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.9.22")
     testImplementation("junit:junit:4.13")
 }

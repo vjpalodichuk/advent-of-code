ad4d5b299d58abbdd4f234ba175136079e8d5817
Vincent Palodichuk <vincent@palodichuk.com>
2024-02-02 06:08:33 -0600

    :bug: Fixed buildSrc test failures.

--- buildSrc/src/test/kotlin/com/capital7software/PluginTest.kt
+++ buildSrc/src/test/kotlin/com/capital7software/PluginTest.kt
@@ -1,42 +1,68 @@
 package com.capital7software
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
-import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
-import java.io.File
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.io.TempDir
 
 abstract class PluginTest {
 
-    @Rule
-    @JvmField
-    val testProjectDir: TemporaryFolder = TemporaryFolder()
-    protected lateinit var settingsFile: File
-    protected lateinit var buildFile: File
+  @TempDir
+  protected lateinit var testProjectDir: Path
+  protected lateinit var settingsFile: File
+  protected lateinit var buildFile: File
+  private lateinit var propertiesFile: File
 
-    @Before
-    fun setup() {
-        settingsFile = testProjectDir.newFile("settings.gradle.kts")
-        settingsFile.appendText("""
+  @BeforeEach
+  fun setup() {
+    settingsFile = Files
+        .createFile(Paths.get(testProjectDir.toString(), "settings.gradle.kts"))
+        .toFile()
+    settingsFile.appendText("""
             rootProject.name = "test"
         """)
-        buildFile = testProjectDir.newFile("build.gradle.kts")
-    }
+    buildFile = Files
+        .createFile(Paths.get(testProjectDir.toString(), "build.gradle.kts"))
+        .toFile()
+    propertiesFile = Files
+        .createFile(Paths.get(testProjectDir.toString(), "gradle.properties"))
+        .toFile()
+    propertiesFile.appendText("""
+      artifactoryUser =
+      artifactoryPassword =
+      artifactoryContextUrl = https://artifactory.capital7software.com/artifactory
+      artifactoryRepoKeyPublishRelease = libs-release-local
+      artifactoryRepoKeyPublishSnapshot = libs-snapshot-local
+      artifactoryRepoKeyReadRelease = libs-release
+      artifactoryRepoKeyReadSnapshot = libs-snapshot
+    """.trimIndent())
+  }
 
-    fun runTask(task: String): BuildResult {
-        return GradleRunner.create()
-                .withProjectDir(testProjectDir.root)
-                .withArguments(task, "--stacktrace")
-                .withPluginClasspath()
-                .build()
-    }
+  fun runTask(task: String): BuildResult {
+    return GradleRunner.create()
+        .withProjectDir(testProjectDir.toFile())
+        .withArguments(task, "--stacktrace")
+        .withPluginClasspath()
+        .build()
+  }
 
-    fun runTaskWithFailure(task: String): BuildResult {
-        return GradleRunner.create()
-                .withProjectDir(testProjectDir.root)
-                .withArguments(task, "--stacktrace")
-                .withPluginClasspath()
-                .buildAndFail()
-    }
+  fun runTaskWithFailure(task: String): BuildResult {
+    return GradleRunner.create()
+        .withProjectDir(testProjectDir.toFile())
+        .withArguments(task, "--stacktrace")
+        .withPluginClasspath()
+        .buildAndFail()
+  }
+
+  fun getAvailableTasks(): BuildResult {
+    return GradleRunner.create()
+        .withProjectDir(testProjectDir.toFile())
+        .withArguments("tasks", "--stacktrace")
+        .withPluginClasspath()
+        .build()
+  }
 }

a8df8556bc48de0827be6e008c57eeff2ec99f50
Vincent Palodichuk <vincent@palodichuk.com>
2024-02-02 02:44:02 -0600

    :zap: Enabled Conventional Commits For Merge.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,23 @@
 rootProject.name = "aoc"
 plugins {
-    id("com.mooltiverse.oss.nyx") version "2.5.1"
+  id("com.mooltiverse.oss.nyx") version "2.5.1"
 }
 
 include("aoc-library", "aoc-2015", "aoc-2023")
 
 configure<com.mooltiverse.oss.nyx.gradle.NyxExtension> {
-    preset = "extended"
+  preset = "extended"
 
-    initialVersion = "1.0.0"
+  initialVersion = "1.0.0"
 
-    changelog {
-        path = "CHANGELOG.md"
-        append = "head"
-    }
+  changelog {
+    path = "CHANGELOG.md"
+    append = "head"
+  }
 
+  commitMessageConventions {
+    enabled.set(mutableListOf("conventionalCommits", "gitmoji", "conventionalCommitsForMerge"))
+  }
 //    releaseTypes {
 //        publicationServices.set(listOf("github"))
 //
@@ -33,13 +36,13 @@ configure<com.mooltiverse.oss.nyx.gradle.NyxExtension> {
 //        }
 //    }
 
-    services.create("github") {
-        type = "GITHUB"
-        options.apply {
-            // The authentication token is read from the GH_TOKEN environment variable.
-            put("AUTHENTICATION_TOKEN", "{{#environmentVariable}}GH_TOKEN{{/environmentVariable}}")
-            put("REPOSITORY_NAME", "advent-of-code")
-            put("REPOSITORY_OWNER", "vjpalodichuk")
-        }
+  services.create("github") {
+    type = "GITHUB"
+    options.apply {
+      // The authentication token is read from the GH_TOKEN environment variable.
+      put("AUTHENTICATION_TOKEN", "{{#environmentVariable}}GH_TOKEN{{/environmentVariable}}")
+      put("REPOSITORY_NAME", "advent-of-code")
+      put("REPOSITORY_OWNER", "vjpalodichuk")
     }
+  }
 }

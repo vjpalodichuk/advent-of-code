609ec4192dd106a31428ce743ccf42a454f46928
Vincent Palodichuk <vincent@palodichuk.com>
2024-02-22 21:43:22 -0600

    :ambulance: Added AOC 2016 Day 16

---
+++ aoc-2016/src/test/kotlin/com/capital7software/aoc/aoc2016aoc/days/Day16Test.kt
@@ -0,0 +1,35 @@
+package com.capital7software.aoc.aoc2016aoc.days
+
+import com.capital7software.aoc.lib.AdventOfCodeTestBase
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class Day16Test : AdventOfCodeTestBase() {
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(Day16Test::class.java)
+  }
+
+  override fun getLogger(): Logger {
+    return log
+  }
+
+  @BeforeEach
+  fun setup() {
+    val instance = Day16()
+    setupFromFile(instance.defaultInputFilename)
+  }
+
+  @Test
+  fun testTimeToPushTheButton() {
+    val length = 20
+    val instance = Day16()
+    val expected = "01100"
+    val actual = instance.generateChecksum(lines.first(), length)
+
+    assertEquals(expected, actual, "The checksum "
+        + "is not what was expected: $expected")
+  }
+}

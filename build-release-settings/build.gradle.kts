4100b59cad6dd31c0bb4e088ebcb856e46b0de76
Vincent Palodichuk <vincent@capital7software.com>
2025-12-26 02:04:12 -0600

    :lock: Fix dependency security issues
    Signed-off-by: Vincent Palodichuk <vincent@capital7software.com>

--- build-release-settings/build.gradle.kts
+++ build-release-settings/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    `kotlin-dsl`
+  `kotlin-dsl`
 }
 
 repositories {
@@ -11,25 +11,38 @@ dependencies {
 }
 
 gradlePlugin {
-    plugins {
-        register("releaseSettings") {
-            id = "com.capital7software.release-settings"
-            implementationClass = "com.capital7software.gradle.plugin.ReleaseSettingsPlugin"
-        }
-        register("releaseRoot") {
-            id = "com.capital7software.release-root"
-            implementationClass = "com.capital7software.gradle.plugin.NyxReleasePlugin"
-        }
+  plugins {
+    register("releaseSettings") {
+      id = "com.capital7software.release-settings"
+      implementationClass = "com.capital7software.gradle.plugin.ReleaseSettingsPlugin"
     }
+    register("releaseRoot") {
+      id = "com.capital7software.release-root"
+      implementationClass = "com.capital7software.gradle.plugin.NyxReleasePlugin"
+    }
+  }
 }
 
 configurations.all {
   this.exclude(group = "ch.qos.logback")
 
-  resolutionStrategy.dependencySubstitution {
-    // Security Fixes!
-    substitute(module("org.apache.commons:commons-lang3:3.1.4"))
-        .using(module("org.apache.commons:commons-lang3:3.20.0"))
-        .because("""https://github.com/vjpalodichuk/advent-of-code/security/dependabot/12""")
+  resolutionStrategy {
+    eachDependency {
+      // Apache Commons (all artifacts)
+      if (requested.group == "org.apache.commons" &&
+          requested.version == "3.1.4"
+      ) {
+        useVersion("3.20.0")
+        because("https://github.com/vjpalodichuk/advent-of-code/security/dependabot/12")
+      }
+
+      // Log4j (all artifacts)
+      if (requested.group == "org.apache.logging.log4j" &&
+          requested.version == "2.25.2"
+      ) {
+        useVersion("2.25.3")
+        because("https://github.com/vjpalodichuk/advent-of-code/security/dependabot/15")
+      }
+    }
   }
 }

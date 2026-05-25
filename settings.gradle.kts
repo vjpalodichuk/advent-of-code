64d7c7178365cdf631fc7640f10d084749417a45
Vincent Palodichuk <vincent@capital7software.com>
2026-05-25 02:00:28 -0500

    :ambulance: fix gitmoji expression to include :sparkle:
    Signed-off-by: Vincent Palodichuk <vincent@capital7software.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ configure<com.mooltiverse.oss.nyx.gradle.NyxExtension> {
     path = "build/CHANGELOG.md"
     append = "" // Recreate the changelog
     sections = mapOf(
-        Pair("Added", "^(feat|:boom:|:sparkles:|:pushpin:|:heavy_plus_sign:)$"),
+        Pair("Added", "^(feat|:boom:|:sparkles:|:sparkle:|:pushpin:|:heavy_plus_sign:)$"),
         Pair("Changed", "^(feat|:boom:|:arrow_up:|:lipstick:|:wrench:|:pencil2:)$"),
         Pair("Fixed", "^(fix|:bug:|:ambulance:)$"),
         Pair("Removed", "^(:fire:|:zap:|:arrow_down:|:heavy_minus_sign:)$"),
@@ -43,6 +43,33 @@ configure<com.mooltiverse.oss.nyx.gradle.NyxExtension> {
           "conventionalCommitsForMerge"
       )
     )
+
+    items.create("conventionalCommitsForMerge") {
+      expression = """(?<type>[a-zA-Z0-9_]+)(!)?(\((?<scope>[a-z ]+)\))?:( (?<title>.+))"""
+      bumpExpressions = mapOf(
+        "patch" to """(?s)(?m)fix(!{0})(\([a-z ]+\))?: (?!.*^(BREAKING( |-)CHANGE: )).*""",
+        "major" to """(?s)(?m)[a-zA-Z0-9_]+(!: .*|.*^(BREAKING( |-)CHANGE: )).*""",
+        "minor" to """(?s)(?m)feat(!{0})(\([a-z ]+\))?: (?!.*^(BREAKING( |-)CHANGE: )).*""",
+      )
+    }
+
+    items.create("gitmoji") {
+      expression = """(?m)^(?<type>:[a-zA-Z0-9_]+:)( (?<title>.+))?$(?s).*"""
+      bumpExpressions = mapOf(
+        "patch" to """(?m)^:(zap|bug|ambulance|lipstick|lock|arrow_down|arrow_up|pushpin|chart_with_upwards_trend|heavy_plus_sign|heavy_minus_sign|wrench|globe_with_meridians|pencil2|rewind|package|alien|bento|wheelchair|speech_balloon|card_file_box|children_crossing|iphone|egg|alembic|mag|label|triangular_flag_on_post|goal_net|dizzy|wastebasket|passport_control|adhesive_bandage):(?s).*""",
+        "major" to """(?m)^:boom:(?s).*""",
+        "minor" to """(?m)^:sparkles:|:sparkle:(?s).*""",
+      )
+    }
+
+    items.create("conventionalCommits") {
+      expression = """(?m)^(?<type>[a-zA-Z0-9_]+)(!)?(\((?<scope>[a-z ]+)\))?:( (?<title>.+))$(?s).*"""
+      bumpExpressions = mapOf(
+        "patch" to """(?s)(?m)^fix(!{0})(\([a-z ]+\))?: (?!.*^(BREAKING( |-)CHANGE: )).*""",
+        "major" to """(?s)(?m)^[a-zA-Z0-9_]+(!: .*|.*^(BREAKING( |-)CHANGE: )).*""",
+        "minor" to """(?s)(?m)^feat(!{0})(\([a-z ]+\))?: (?!.*^(BREAKING( |-)CHANGE: )).*""",
+      )
+    }
   }
 
   releaseTypes {
